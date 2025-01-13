078477972bab852e8d455824bfe53c956e2ce6de
Ryan6y <xryyu@ucdavis.edu>
2025-01-13 14:02:20 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -3,15 +3,11 @@
 import math
 
 print('Hello, again') # greeting
-print(1.5e-2)
-print(1 + 1)
-print(2**3)
-print(pow(2, 3))
-print(math.pow(2,3))
-print(2**0.5)
-print(math.sqrt(2))
-print(math.log(2))
-print(0.1 * 1)
+
+def gougu(a, b): return  math.sqrt(a**2 + b**2)
+
+da_an = gougu(3, 4)
+print(da_an)
 
 #print(1 / 0)		# divide by zero error
 #print(math.log(0))	# math domain error
