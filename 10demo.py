1cc228b3ba4f44da67037474a510063eb4cb4306
Ryan6y <xryyu@ucdavis.edu>
2025-01-19 22:58:40 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -2,18 +2,13 @@
 
 import math
 
-print('Hello, again') # greeting
+print('hello, again') # greeting
 
-def gougu(a, b): return  math.sqrt(a**2 + b**2)
+s = 'hello world'
+print(s, type(s))
 
-da_an = gougu(3, 4)
-print(da_an)
-
-#print(1 / 0)		# divide by zero error
-#print(math.log(0))	# math domain error
-#print(math.sqrt(-1))	# math domain error
-
-a = 3				# side of triangle
-b = 4				# side of triangle
-c = math.sqrt(a**2 + b**2)	# hypotenuse
-print(c)
+a = 2
+b = 2
+if a == b:
+	print('a equals b')
+	
