4870d20d35253529271ffd89058dc42d89b05d7f
Ryan6y <xryyu@ucdavis.edu>
2025-01-21 13:23:41 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -7,12 +7,22 @@ print('hello, again') # greeting
 s = 'hello world'
 print(s, type(s))
 
-a = 2
-b = 2
-if a == b:
-	print('a equals b')
-	print(a, b)
+a = 0.3
+b = 0.1 * 3
+if   a < b: print('a < b')
+elif a > b: print('a > b')
+else:       print('a == b')
 
-c = 3
-d = 4
+print(abs(a - b)) # 5.551115123125783e-17
+if abs(a - b) < 1e-9: print('close enough')
+
+def circle_area(r): return math.pi * r**2
+def rectangle_area(w, h): return w * h
+def triangle_area(w, h): return rectangle_area(w, h) / 2
+r=2
+w=3
+h=4
+print (circle_area(r))
+print (rectangle_area(w, h))
+print (triangle_area(w, h))
 
