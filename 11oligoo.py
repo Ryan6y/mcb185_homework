e548b57e78612fc49a6cd92e7366fdf7c3c48595
Ryan6y <xryyu@ucdavis.edu>
2025-01-21 16:23:35 -0800

    update

--- 11oligoo.py
+++ 11oligoo.py
@@ -1,10 +1,10 @@
 def tm(a, c, g, t):
-  total_bases = a + c + g + t
+  length = a + c + g + t
 	
-  if total_bases <= 13:
-    tm= (a + t) * 2 + (g + c) * 4
+  if length <= 13:
+    tm= (a + t) * 2 + (g + c) * 4 #for short
   else:
-    tm = 64.9 + 41 * (g + c - 16.4) / total_bases 
+    tm = 64.9 + 41 * (g + c - 16.4) / length #for long
   return tm
 
 print(tm(5, 7, 3, 4))
