3367d9013c6dc23032724f248ae00f6eb905944b
Ryan6y <xryyu@ucdavis.edu>
2025-02-11 01:44:09 -0800

    update

---
+++ 21fizzbuzz.py
@@ -0,0 +1,11 @@
+#21fizzbuzz.py by Xiaoran Yu
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
