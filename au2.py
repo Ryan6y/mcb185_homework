1aec1f5ecc6e04cf573538e44af639d3760eb221
Ryan6y <xryyu@ucdavis.edu>
2025-02-11 15:43:46 -0800

    update

--- au2.py
+++ au2.py
@@ -1,6 +1,6 @@
 i = 51
 
-while i >= 3:
+while i >= 11:
     i = i - 2
     if i % 2 == 0:
         print(i)
@@ -8,6 +8,12 @@ while i >= 3:
         print(i)
     elif i % 5 == 0:
         print(i)
+    elif i % 7 == 0:
+        print(i)
     else:
         print(f"{i}*")
+print("7*")
+print("5*")
+print("3*")
+print("1*")
     
