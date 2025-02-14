5181e5b2bbf9b4d304dc365a2ca3073436bc930d
Ryan6y <xryyu@ucdavis.edu>
2025-02-13 16:05:32 -0800

    update

--- 2demo.py
+++ 2demo.py
@@ -1,9 +1,21 @@
-for i in range (1, 101, +1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
-    elif i % 5 == 0:
-        print("buzz")
-    elif i % 3 == 0:
-        print("fizz")
+import random
+import math
+
+def dis(x1, y1, x2, y2):
+    d = math.sqrt((x1 - x2)** 2 + (y1 - y2)** 2)
+    return d 
+
+inside = 0
+outside = 0
+
+for i in range(100000):
+    x = random.random()
+    y = random.random()
+    d = dis(0, 0, x, y)
+
+    if d <= 1:
+        inside += 1
     else:
-        print(i)
+        outside += 1
+
+    print(inside/(outside+inside)*4)
