9ed67665713d9e8bada011ddc29db6ed7212f3c3
Ryan6y <xryyu@ucdavis.edu>
2025-02-20 15:09:07 -0800

    update

--- 30demo.py
+++ 30demo.py
@@ -1,20 +1,35 @@
-import random
-import sys
+import math
 
-trials = int(sys.argv[1])
-days = int(sys.argv[2])
-people = int(sys.argv[3])
+s = 'hello world'
+print(s)
 
-matches = 0
+s1 = 'hey "dude"'
+s2 = "don't tell me what to do"
+print(s1, s2)
 
-for _ in range(trials):
-    birthdays = []
-    for _ in range(people):
-        birthday = random.randint(0, days - 1)
-        birthdays.append(birthday)
-    if len(birthdays) != len(set(birthdays)):
-        matches += 1
-        print(f"{len(birthdays)}")
-        print(f"{len(set(birthdays))}")
-probability = matches / trials
-print(f"Probability of at least two people sharing a birthday: {probability:.4f}")
+print(f"{len(s1)}")
+
+print(s.upper())
+print(s)
+
+print(s.replace('o', ''))
+print(s.replace('o', '').replace('r', 'i'))
+
+print(f'{math.pi}')            # does nothing special
+print(f'{math.pi:.3f}')        # 3 fixed digits after decimal
+print(f'{1e6 * math.pi:e}')    # exponent notation
+print(f'{"hello world":>20}')  # right justify with space filler
+print(f'{"hello world":.>20}') # right justify with dot filler
+print(f'{20:<10} {10}')        # left justify
+
+print('{} {:.3f}'.format('str.format', math.pi))
+print('%s %.3f' % ('printf', math.pi))
+
+nts = ['A', 'T', 'C']
+print(nts)
+nts[2] = 'G'
+print(nts)
+
+#practice
+
+abc = [1, 2, 3, 4, 5, 6]
