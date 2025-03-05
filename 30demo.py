02417a4b721ef3bd8124f67cb0e055c2f37cdfda
Ryan6y <xryyu@ucdavis.edu>
2025-03-04 16:16:42 -0800

    update

--- 30demo.py
+++ 30demo.py
@@ -1,7 +1,20 @@
 import sys
 
 list=[]
-aa = sys.argv[1]
-count = len(aa)
+for arg in sys.argv[1]:
+    list.append(arg)
 
-print(count)
+count = len(list)
+for i in range(count):
+    if (i+1) % 3 == 1:
+    	rf = 1
+    elif (i+1) % 3 == 2:
+    	rf = 2
+    elif (i+1) % 3 == 0:
+    	rf = 3
+    ls = list[i:(i+3)]
+    codon = ''.join(ls)
+    print(f"{i+1}\t{rf}\t{codon}")
+    if i + 3 >= count:
+        break
+
