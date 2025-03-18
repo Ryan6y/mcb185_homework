e2d5a7839fadc97bd5cd99d0c11e682647706534
Ryan6y <xryyu@ucdavis.edu>
2025-03-18 15:32:57 -0700

    update

--- 45colorname.py
+++ 45colorname.py
@@ -1,10 +1,37 @@
 import sys
 
+def dtc(P, Q):
+    d = 0
+    for p, q in zip(P, Q):
+        d += abs(p - q)
+    return d
+
 colorfile = sys.argv[1]
 R = int(sys.argv[2])
 G = int(sys.argv[3])
 B = int(sys.argv[4])
+input_color = (R, G, B)
 
-with open(colorfile) as fp:
+min_dis = 0
+closest_color = 0
+
+with open(colorfile, 'rt') as fp:
     for line in fp:
-        print(line)
+        
+        line = line.strip()
+        parts = line.split()
+        name = parts[0]
+        rgb_code = parts[2].split(',')
+        
+        r = int(rgb_code[0])
+        g = int(rgb_code[1])
+        b = int(rgb_code[2])
+        
+        color_value = (r, g, b)
+        dis = dtc(input_color, color_value)
+        
+        if min_dis == 0 or dis < min_dis:
+            min_dis = dis
+            closest_color = name
+
+print(closest_color)
