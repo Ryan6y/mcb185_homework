964599552e80a71e666fa5c65dfdfae41b3888e4
ryan6y <xryyu@ucdavis.edu>
2025-10-25 19:57:49 -0700

    update

--- 10demo.py
+++ 10demo.py
@@ -1,36 +1,10 @@
-# 10demo.py by Xiaoran Yu
+#10demo by Ryan6y
 
-import math
+def char_to_prob(a):
+    return ord(a)
 
-print('hello, again') # greeting
+def prob_to_char(b):
+    return chr(b)
 
-s = 'hello world'
-print(s, type(s))
-
-a = 0.3
-b = 0.1 * 3
-print (a, b)
-if   a < b: print('a < b')
-elif a > b: print('a > b')
-else:       print('a == b')
-
-print(abs(a - b)) # 5.551115123125783e-17
-if abs(a - b) < 1e-9: print('close enough')
-
-def circle_area(r): return math.pi * r**2
-def rectangle_area(w, h): return w * h
-def triangle_area(w, h): return rectangle_area(w, h) / 2
-r=2
-w=3
-h=4
-print (circle_area(r))
-print (rectangle_area(w, h))
-print (triangle_area(w, h))
-
-def max3(a, b, c):
-	if a > b > c: return a
-	elif a < b < c: return c
-	else : return b
-print(max3(1,2,3))
-print(max3(3,2,1))
-print(max3(5,9,2))
+print(char_to_prob('A'))
+print(prob_to_char(65))
--- 20test.py
+++ 20test.py
@@ -1,45 +1,9 @@
-##Unit1 assesment by Xiaoran Yu and Reese Carlsom
+#20demo by Xiaoran
 
-#first question
-import math
+import sys
 
-def dis(x1, y1, x2, y2):
-    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+a = int(sys.argv[1])
 
-print(dis(0, 0, 3, 4))
-print(dis(0, 0, 2, 2))
+b = a * 2
 
-#second question
-def vp(a):
-    if a <= 1 and a >= 0:
-    	return 'valid'
-    else: 
-    	return 'invalid'
-
-print(vp(9))
-print(vp(0.345))
-print(vp(-0.1))
-
-#Third question
-def pos2rf(a):
-    if a % 3 == 1:
-    	return 'Reading Frame 1'
-    if a % 3 == 2:
-    	return 'Reading Frame 2'
-    if a % 3 == 0:
-    	return 'Reading Frame 3'
-
-print(pos2rf(1))
-print(pos2rf(1000))
-print(pos2rf(9))
-
-#Forth Question
-def maxin4(a, b, c, d):
-	if a > b > c > d: return a
-	if b > c > d: return b
-	if c > d: return c
-	else: return d
-
-print(maxin4(1, 2, 3, 4))
-print(maxin4(3, 4, 100, 22))
-print(maxin4(10000, 2, 345, 777))
+print(b)
